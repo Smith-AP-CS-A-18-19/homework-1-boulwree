f25750c0c506ca48ba757f9d55093b9d285b575a
Reece Boulware <boulwree@berkeleyprep.org>
2018-09-06 10:00:55 -0400

    I did it!

--- Homework1.java
+++ Homework1.java
@@ -1,5 +1,5 @@
 /*
- * First and Last Names
+ * Reece Boulware
  */
 
 import java.awt.Rectangle;
@@ -11,7 +11,7 @@ public class Homework1 {
 	public static int problem1() {
 		int mystery = 1;
 		mystery = mystery + 1;
-		int mystery = 1 - 2 * mystery;
+		mystery = 1 - 2 * mystery;
 		return mystery;
 	}
 
@@ -19,24 +19,35 @@ public class Homework1 {
 	 * height of h
 	 */
 	public static Rectangle problem2(int w, int h) {
-
+		Rectangle rect = new Rectangle(40, 30, w, h);
+		return rect;
 	}
 
 	/* Declare an integer, assign it a value of 5, then return it
 	 */
 	public static int problem3() {
+		int x = 5;
+		return x;
 
 	}
 
 	/* Declare and return a String containing the phrase "Computer Science"
 	 */
 	public static String problem4() {
+		String x = "Computer Science";
+		return x;
+
+
 
 	}
 
 	/* Add 5 to num and return the new value
 	 */
 	public static int problem5(int num) {
+		int x = num + 5;
+		return x;
+
+
 
 	}
 
